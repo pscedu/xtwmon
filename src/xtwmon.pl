18399bd30f4ba05804bb2db59ebe65ec67e60b55
yanovich <>
2005-07-28 20:44:17 +0000

    more initial proper job data structure referencing; flip y values for the x and z planes; flesh drawing the actual nodes correctly; add states and their colors

--- src/xtwmon.pl
+++ src/xtwmon.pl
@@ -17,6 +17,12 @@ use constant DIM_X => 0;
 use constant DIM_Y => 1;
 use constant DIM_Z => 2;
 
+use constant ST_FREE	=> 0;
+use constant ST_DOWN	=> 1;
+use constant ST_USED	=> 2;
+use constant ST_SERV	=> 3;
+use constant ST_UNAC	=> 4;
+
 use constant SLEEP_INTV => 5000;
 
 use constant ST_MTIME => 9;
@@ -24,10 +30,18 @@ use constant ST_MTIME => 9;
 my $out_dir = "../www/latest";
 my $archive_tmpl = "../www/data-%Y-%m-%d-tm-%H-%M";
 
-my %dimcol = (
-	DIM_X() => "red",
-	DIM_Y() => "green",
-	DIM_Z() => "blue"
+my @dimcol = (
+	"red",			# x
+	"green",		# y
+	"blue"			# z
+);
+
+my @statecol = (
+	[255, 255, 255],	# FREE
+	[0, 0, 0],		# DOWN
+	[0, 0, 0],		# USED
+	[255, 255, 0],		# SERV
+	[51, 51, 255],		# UNAC
 );
 
 # end config
@@ -36,7 +50,7 @@ my @max;
 my @nodes;
 my @invmap;
 
-parse_wimap(\@max, \@nodes);
+parse_wimap(\@max);
 
 for (;;) {
 	if (-d $out_dir) {
@@ -67,17 +81,26 @@ exit;
 }
 
 sub cen {
-	my ($img, $str, $sx, $ex, $y, $col) = @_;
+	my ($img, $str, $sx, $ex, $sy, $ey, $col, $ceny) = @_;
 	my $len = length($str);
 #	$img->string($fn, ($sx + $ex) / 2 - $fn->width * $len / 2,
 #	    $y + $fn->height, $str, $col);
 	my $d_img = GD::Image->new(100, 100);
 	my $fn = "/usr/X11R6/lib/X11/fonts/TTF/tahoma.ttf";
-	my @bnd = $d_img->stringFT($col, $fn, 10, 0, 0, 50, $str);
+	my $sz = 8;
+	my @bnd = $d_img->stringFT($col, $fn, $sz, 0, 0, 50, $str);
 	my $ren_w = $bnd[2] - $bnd[0];
 	my $ren_h = $bnd[1] - $bnd[7];
-	$img->stringFT($col, $fn, 10, 0,
-	    ($sx + $ex) / 2 - $ren_w / 2, $y + $ren_h, $str);
+
+	my $y;
+	if ($ceny) {
+		$y = ($sy + $ey) / 2 + $ren_h / 2 - 1;
+	} else {
+		$y = $sy + $ren_h;
+	}
+
+	$img->stringFT($col, $fn, $sz, 0,
+	    ($sx + $ex) / 2 - $ren_w / 2, $y, $str);
 	return ($ren_w, $ren_h);
 }
 
@@ -89,7 +112,9 @@ sub gen {
 	my %col = (
 		"red"	=> $img->colorAllocate(255, 0, 0),
 		"green"	=> $img->colorAllocate(0, 255, 0),
-		"blue"	=> $img->colorAllocate(0, 0, 255)
+		"blue"	=> $img->colorAllocate(0, 0, 255),
+		"white"	=> $col_white,
+		"black"	=> $col_black
 	);
 
 	my @labels = (
@@ -106,23 +131,62 @@ sub gen {
 
 	$img->fill(1, 1, $col_white);
 	my ($used_w, $used_h) = cen($img, $labels[$dim], 0, IMG_WIDTH,
-	    0, $col_black);
+	    0, 0, $col_black, 0);
 	$used_h += 2; # Text border
 
 	my ($udim, $vdim) = @{ $xdim[$dim] };
 
-	my $uincr = IMG_WIDTH / $max[$udim];
-	my $vincr = (IMG_HEIGHT - $used_h) / $max[$vdim];
+	my $uincr = IMG_WIDTH / ($max[$udim] + 1);
+	my $vincr = (IMG_HEIGHT - $used_h) / ($max[$vdim] + 1);
 	my $node_w = $uincr - 10;
 	my $node_h = $vincr - 10;
 
+	my ($upstart, $vpstart);
+	$upstart = 0;
+	$vpstart = $used_h;
+
 	my ($u, $up, $v, $vp);
-	for ($u = 0, $up = 0; $u < $max[$udim]; $u++, $up += $uincr) {
-		for ($v = 0, $vp = $used_h; $v < $max[$vdim]; $v++, $vp += $vincr) {
+	my ($_x, $_y, $_z);
+	if ($dim == DIM_X) {
+		$_x = \$pos;
+		$_y = \$v;
+		$_z = \$u;
+		$vincr *= -1;
+		$vpstart = IMG_HEIGHT + $vincr;
+	} elsif ($dim == DIM_Y) {
+		$_x = \$u;
+		$_y = \$pos;
+		$_z = \$v;
+	} elsif ($dim == DIM_Z) {
+		$_x = \$u;
+		$_y = \$v;
+		$_z = \$pos;
+		$vincr *= -1;
+		$vpstart = IMG_HEIGHT + $vincr;
+	}
+
+	for ($u = 0, $up = $upstart;
+	    $u < $max[$udim] + 1;
+	    $u++, $up += $uincr) {
+		for ($v = 0, $vp = $vpstart;
+		    $v < $max[$vdim] + 1;
+		    $v++, $vp += $vincr) {
+			my $node = $nodes[$$_x][$$_y][$$_z];
+			unless ($node) {
+				$node = {
+					nid => -1,
+					col => $statecol[ST_UNAC]
+				};
+			}
+			my $col = $node->{col};
+			$col = $$col if ref $col eq "REF";
 			$img->filledRectangle($up, $vp,
-			    $up + $node_w, $vp + $node_h, $col{$dimcol{$dim}});
+			    $up + $node_w, $vp + $node_h,
+			    $img->colorAllocate(@$col));
 			$img->rectangle($up, $vp,
 			    $up + $node_w, $vp + $node_h, $col_black);
+			cen($img, $node->{nid}, $up, $up + $node_w,
+			    $vp, $vp + $node_h, $col_black, 1);
 		}
 	}
 
@@ -138,9 +202,8 @@ sub err {
 }
 
 sub parse_wimap {
-	my ($max, $nodes) = @_;
+	my ($max) = @_;
 
-	$nodes = [];
 	@$max = (0, 0, 0);
 
 	local ($_, *WIMAP);
@@ -161,12 +224,12 @@ sub parse_wimap {
 
 		my ($nid, $cb, $r, $cg, $m, $n, $x, $y, $z) = @m;
 
-		$nodes->[$x] = [] unless ref $nodes->[$x] eq "ARRAY";
-		$nodes->[$x][$y] = [] unless ref $nodes->[$x][$y] eq "ARRAY";
-		$invmap[$nid] = $nodes->[$x][$y][$z] = {
-			nid => $nid
+		$nodes[$x] = [] unless ref $nodes[$x] eq "ARRAY";
+		$nodes[$x][$y] = [] unless ref $nodes[$x][$y] eq "ARRAY";
+		$invmap[$nid] = $nodes[$x][$y][$z] = {
+			nid => $nid,
+			col => $statecol[ST_FREE]
 		};
-
 		$max->[DIM_X] = $x if $x > $max->[DIM_X];
 		$max->[DIM_Y] = $y if $y > $max->[DIM_Y];
 		$max->[DIM_Z] = $z if $z > $max->[DIM_Z];
@@ -174,7 +237,7 @@ sub parse_wimap {
 	close WIMAP;
 }
 
-my @jobs;
+my %jobs;
 
 sub parse_jobmap {
 	local ($_, *JMAP);
@@ -192,19 +255,31 @@ sub parse_jobmap {
 		next unless @m;
 		my ($nid, $enabled, $jobid) = @m;
 		next unless $invmap[$nid];
-		$invmap[$nid]{job} = job_get($jobid);
+		my $j = $invmap[$nid]{job} = job_get($jobid);
+		my $node = $invmap[$nid];
+
+		if ($enabled == 0) {
+			$node->{col} = $statecol[ST_DOWN];
+		} elsif ($jobid) {
+			$node->{col} = \$j->{col};
+		} else {
+			$node->{col} = $statecol[ST_FREE];
+		}
 	}
 	close JMAP;
 
-	for (my $j = 0; $j < @jobs; $j++) {
-		$jobs[$j]{col} = col_get($j, scalar @jobs);
+	my @keys = keys %jobs;
+	my $len = scalar @keys;
+	my $t = 0;
+	foreach my $j (@keys) {
+		$jobs{$j}{col} = col_get($t++, $len);
 	}
 }
 
 sub job_get {
 	my ($jobid) = shift;
-	return $jobs[$jobid] if $jobs[$jobid];
-	$jobs[$jobid] = {
+	return $jobs{$jobid} if $jobs{$jobid};
+	$jobs{$jobid} = {
 		id => $jobid,
 	};
 }
