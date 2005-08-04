880eb84d3cc2b11040a98e723aae29c3a4eda7c5
yanovich <>
2005-08-04 14:34:53 +0000

    rework to not generate tmp files

--- lib/XTWMon/Plot.pm
+++ lib/XTWMon/Plot.pm
@@ -11,17 +11,15 @@ use warnings;
 # Imporant files
 use constant _PATH_GNUPLOT	=> "/usr/bin/gnuplot";
 use constant _PATH_STDIN	=> "/dev/stdin";
-use constant kLayoutFile	=> "/var/www/html/xtwmon/src/data/rtrtrace";
-use constant kNidsListFile	=> "/var/www/html/xtwmon/src/data/nids_list_phantom";
 
-use constant kGnuPlotFile	=> "/tmp/config.gp";
-use constant kDisabledFile	=> "/tmp/disabled";
-use constant kFreeFile		=> "/tmp/free";
-use constant kJobPrefix		=> "/tmp/jid_";
+# Must be absolute, since mod_perl puts you in strange places.
+use constant _PATH_DISABLED	=> "/var/www/html/xtwmon/src/data/disabled";
+use constant _PATH_FREE		=> "/var/www/html/xtwmon/src/data/free";
+use constant _PATH_JOBPREFIX	=> "/var/www/html/xtwmon/src/data/jid_";
 
 use constant kTitle => "XT3 Wired View";
 
-# XXX This should be read through sys.argv
+# Defaults to gnuplot
 use constant kRotX => 60;
 use constant kRotZ => 20;
 use constant kScalX => 1;
@@ -32,24 +30,16 @@ sub new {
 	my $pkg = ref($class) || $class;
 	return bless {
 		r	=> $r,
-		nodes	=> [],
-		jobs	=> {},
-		dis	=> 1,
-		free	=> 0,
 	}, $pkg;
 }
 
 # Create a gnuplot file
 sub gnu_plot {
 	my ($obj, $rx, $rz, $sx, $sz) = @_;
+	local (*IN, *OUT, *ERR);
 
 	my $title = kTitle;
 
-	local (*IN, *OUT, *ERR);
-
-	# Hack: programs are not allowed to write to
-	# their stdout, but stderr works fine, so
-	# redirect to there.
 	my $h = IPC::Run::start([_PATH_GNUPLOT, _PATH_STDIN],
 	    "<pipe", \*IN, ">pipe", \*OUT, "2>pipe", \*ERR)
 	    or $obj->err(_PATH_GNUPLOT);
@@ -73,76 +63,10 @@ EOF
 # create the list of files that should be passed
 # as arguments to gnuplot
 sub file_list {
-	my ($obj) = @_;
-
-	# string the files together
-	my $list = "'" . join("','", values %{ $obj->{jobs} }) . "'";
-
-	# Now add disabled & free nodes if there were any
-	$list .= ",'" . kDisabledFile . "'"	if $obj->{dis};
-	$list .= ",'" . kFreeFile . "'"		if $obj->{free};
-	return $list;
-}
-
-# delete the temporary files
-sub clear_files {
-	my ($obj) = @_;
-
-	unlink $_ foreach (values %{ $obj->{jobs} });
-
-	unlink kDisabledFile	if $obj->{dis};
-	unlink kFreeFile	if $obj->{free};
-}
-
-# Obtain the (x,y,z) coordinates of every node
-sub parse_layout {
-	my ($obj, $fin) = @_;
-
-	local $_;
-	open FP, $fin or $obj->err($fin);
-	while (<FP>) {
-		chomp;
-		my ($nid, $lay, $pos) = split;
-		my ($x, $y, $z) = split(/,/, $pos);
-
-		$obj->{nodes}[$nid] = "$x $y $z";
-	}
-	close FP;
-}
-
-# parse the nidslist file and create a file
-# for every jobid and add the node position
-sub parse_nidslist {
-	my ($obj, $fin) = @_;
-
-	my $fn;
-	local $_;
-	open FP, $fin or $obj->err($fin);
-	while (<FP>) {
-		chomp;
-		my ($nid, $active, $jobid) = split;
-
-		if ($jobid != 0) {
-			# Active job
-			$fn = kJobPrefix . $jobid;
-
-			# Store the filename (prefix not needed)
-			# It is relative to the config file
-			$obj->{jobs}{$jobid} = $fn;
-		} elsif ($active == 0) {
-			# Disabled node
-			$fn = kDisabledFile;
-			$obj->{dis} = 1;
-		} else {
-			# Free Node
-			$fn = kFreeFile;
-			$obj->{free} = 1;
-		}
-		open FP2, ">> $fn" or err($fn);
-		print FP2 "$obj->{nodes}[$nid]\n";
-		close FP2;
-	}
-	close FP;
+	my @files = glob(_PATH_JOBPREFIX . "*");
+	push @files, _PATH_FREE		if -e _PATH_FREE;
+	push @files, _PATH_DISABLED	if -e _PATH_DISABLED;
+	return ("'" . join("','", @files) . "'");
 }
 
 sub err {
@@ -158,15 +82,8 @@ sub main {
 	$obj->{r}->content_type('image/png');
 	my ($rx, $rz, $sx, $sz) = (kRotX, kRotZ, kScalX, kScalZ);
 
-	# Parse files
-	$obj->parse_layout(kLayoutFile);
-	$obj->parse_nidslist(kNidsListFile);
-
 	# Run the plot
 	$obj->gnu_plot($rx, $rz, $sx, $sz);
-
-	# Clear the data files
-#	$obj->clear_files();
 }
 
 1;
