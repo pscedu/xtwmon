09c6f5672fa8e2c6db085c6c7eb874128f35d2b2
yanovich <>
2005-08-04 15:37:53 +0000

    add labels to plots; add code to set x,y,z and view

--- lib/XTWMon/Plot.pm
+++ lib/XTWMon/Plot.pm
@@ -13,9 +13,9 @@ use constant _PATH_GNUPLOT	=> "/usr/bin/gnuplot";
 use constant _PATH_STDIN	=> "/dev/stdin";
 
 # Must be absolute, since mod_perl puts you in strange places.
-use constant _PATH_DISABLED	=> "/var/www/html/xtwmon/src/data/disabled";
-use constant _PATH_FREE		=> "/var/www/html/xtwmon/src/data/free";
-use constant _PATH_JOBPREFIX	=> "/var/www/html/xtwmon/src/data/jid_";
+use constant _PATH_DISABLED	=> "/var/www/html/xtwmon/www/latest/disabled";
+use constant _PATH_FREE		=> "/var/www/html/xtwmon/www/latest/free";
+use constant _PATH_JOBPREFIX	=> "/var/www/html/xtwmon/www/latest/jid_";
 
 use constant kTitle => "XT3 Wired View";
 
@@ -30,6 +30,13 @@ sub new {
 	my $pkg = ref($class) || $class;
 	return bless {
 		r	=> $r,
+		rx	=> kRotX,
+		rz	=> kRotZ,
+		sx	=> kScalX,
+		sz	=> kScalZ,
+		x	=> 0,
+		y	=> 0,
+		z	=> 0
 	}, $pkg;
 }
 
@@ -64,9 +71,16 @@ EOF
 # as arguments to gnuplot
 sub file_list {
 	my @files = glob(_PATH_JOBPREFIX . "*");
-	push @files, _PATH_FREE		if -e _PATH_FREE;
-	push @files, _PATH_DISABLED	if -e _PATH_DISABLED;
-	return ("'" . join("','", @files) . "'");
+	my @labels = map { 'job ' . join '', /jid_(\d+)$/ } @files;
+	push(@files, _PATH_FREE),     push @labels, 'free'	if -e _PATH_FREE;
+	push(@files, _PATH_DISABLED), push @labels, 'disabled' if -e _PATH_DISABLED;
+	my $files = "";
+	my $n;
+	for ($n = 0; $n < @files; $n++) {
+		$files .= qq{'$files[$n]' title '$labels[$n]', };
+	}
+	$files =~ s/, $//;
+	return ($files);
 }
 
 sub err {
@@ -83,7 +97,22 @@ sub main {
 	my ($rx, $rz, $sx, $sz) = (kRotX, kRotZ, kScalX, kScalZ);
 
 	# Run the plot
-	$obj->gnu_plot($rx, $rz, $sx, $sz);
+	$obj->gnu_plot();
+}
+
+sub setview {
+	my ($obj, $rx, $rz, $sx, $sz) = @_;
+	$obj->{rx} = $rx if defined $rx && $rx =~ /^\d+$/;
+	$obj->{rz} = $rz if defined $rz && $rz =~ /^\d+$/;
+	$obj->{sx} = $sx if defined $sx && $sx =~ /^\d+$/;
+	$obj->{sz} = $sz if defined $sz && $sz =~ /^\d+$/;
+}
+
+sub setpos {
+	my ($obj, $x, $y, $z) = @_;
+	$obj->{x} = $x if defined $x && $x =~ /^\d+$/;
+	$obj->{y} = $y if defined $y && $y =~ /^\d+$/;
+	$obj->{z} = $z if defined $z && $z =~ /^\d+$/;
 }
 
 1;
