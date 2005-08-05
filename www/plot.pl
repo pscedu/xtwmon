bc015d23da2847c8a6bf54bd6d56c458aaec1b9f
yanovich <>
2005-08-05 16:56:11 +0000

    simplify some things

--- www/plot.pl
+++ www/plot.pl
@@ -6,10 +6,20 @@ use warnings;
 use XTWMon::Plot;
 use CGI;
 
+my $r = shift;
 my $cgi = CGI->new();
 
-my $p = XTWMon::Plot->new(shift);
-$p->setpos($cgi->param("x"), $cgi->param("y"), $cgi->param("z"));
-$p->setview($cgi->param("rx"), $cgi->param("rz"),
-    $cgi->param("sx"), $cgi->param("sz"));
-$p->main();
+my ($x, $y, $z, $rx, $rz, $sx, $sz);
+$x = $cgi->param("x");
+$y = $cgi->param("y");
+$z = $cgi->param("z");
+$rx = $cgi->param("rx");
+$rz = $cgi->param("rz");
+$sx = $cgi->param("sx");
+$sz = $cgi->param("sz");
+
+my $p = XTWMon::Plot->new();
+$p->setpos($x, $y, $z);
+$p->setview($rx, $rz, $sx, $sz);
+$r->content_type('image/png');
+$p->gnu_plot();
