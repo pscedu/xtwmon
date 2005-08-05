45924aba61eba4469b5cd3b6634a94e9b6b7cd3f
yanovich <>
2005-08-05 13:32:27 +0000

    pass cgi params for x,y,z and rot/scale to Plot

--- www/plot.pl
+++ www/plot.pl
@@ -4,6 +4,12 @@
 use strict;
 use warnings;
 use XTWMon::Plot;
+use CGI;
+
+my $cgi = CGI->new();
 
 my $p = XTWMon::Plot->new(shift);
+$p->setpos($cgi->param("x"), $cgi->param("y"), $cgi->param("z"));
+$p->setview($cgi->param("rx"), $cgi->param("rz"),
+    $cgi->param("sx"), $cgi->param("sz"));
 $p->main();
