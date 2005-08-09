3292159decc57ba659f10173b4a630d3eaee8fb3
yanovich <>
2005-08-09 20:33:14 +0000

    selnode one node at a time, we need provisions for multiselnodes; add job shit

--- www/main.js
+++ www/main.js
@@ -11,10 +11,14 @@ function adj() {
 
 }
 
+var selparent = null
+
 function selnode(dim, nid, sx, sy, ex, ey) {
 	var e_img = getimg('img' + dim)
 
 	if (e_img && document.createElement) {
+		if (selparent && selparent.firstChild)
+			selparent.removeChild(selparent.firstChild)
 		var e_cell = e_img.parentNode
 		var e_div = document.createElement('div')
 		var e_pdiv = document.createElement('div')
@@ -37,6 +41,8 @@ function selnode(dim, nid, sx, sy, ex, ey) {
 		e_cell.removeChild(e_img)
 		e_cell.appendChild(e_pdiv)
 		e_cell.appendChild(e_img)
+
+		selparent = e_cell
 	}
 }
 
@@ -52,4 +58,12 @@ function getimg(name) {
 	if (document.images)
 		return (document.images[name])
 	return (null)
+}
+
+var jobs = []
+var invjmap = []
+
+function Job(id) {
+	jobs[jobs.length] = invjmap[id] = this
+	return (this)
 }
