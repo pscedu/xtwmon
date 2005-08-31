2c440f1aa08b824f755eec6c469194bbbbaeaacb
yanovich <>
2005-08-31 19:25:20 +0000

    revise to use xt3dmon

--- lib/XTWMon/Plot.pm
+++ lib/XTWMon/Plot.pm
@@ -6,99 +6,57 @@ package XTWMon::Plot;
 use lib qw(..);
 use XTWMon;
 use CGI;
-use IPC::Run;
+use IO::Socket;
 use strict;
 use warnings;
 
-# Imporant files
-use constant _PATH_GNUPLOT	=> "/usr/bin/gnuplot";
-use constant _PATH_STDIN	=> "/dev/stdin";
+use constant XT3D_HOST => "localhost";
+use constant XT3D_PORT => 24242;
 
-use constant kTitle => "XT3 Wired View";
+use constant DEF_X  => -55.00;
+use constant DEF_Y  =>  38.00;
+use constant DEF_Z  =>  70.00;
+use constant DEF_LX =>   0.86;
+use constant DEF_LY =>  -0.24;
+use constant DEF_LZ =>  -0.45;
 
-# Defaults to gnuplot
-use constant kRotX => 60;
-use constant kRotZ => 20;
-use constant kScalX => 1;
-use constant kScalZ => 1;
+use constant WIDTH => 500;
+use constant HEIGHT => 300;
 
 sub new {
 	my ($class) = @_;
 	my $pkg = ref($class) || $class;
 	return bless {
-		rx	=> kRotX,
-		rz	=> kRotZ,
-		sx	=> kScalX,
-		sz	=> kScalZ,
-		x	=> 0,
-		y	=> 0,
-		z	=> 0,
+		x	=> DEF_X,
+		y	=> DEF_Y,
+		z	=> DEF_Z,
+		lx	=> DEF_LX,
+		ly	=> DEF_LY,
+		lz	=> DEF_LZ,
 		jobs	=> []
 	}, $pkg;
 }
 
-# Create a gnuplot file
-sub gnu_plot {
+sub print {
 	my ($obj) = @_;
-	local (*IN, *OUT, *ERR);
-
-	my $title = kTitle;
-
-	my $h = IPC::Run::start([_PATH_GNUPLOT, _PATH_STDIN],
-	    "<pipe", \*IN, ">pipe", \*OUT, "2>pipe", \*ERR)
-	    or $obj->err(_PATH_GNUPLOT);
-	print IN <<EOF;
-set terminal png small color picsize 500 300
-set view $obj->{rx}, $obj->{rz}, $obj->{sx}, $obj->{sz}
-set xlabel "X"
-set ylabel "Y"
-set zlabel "Z"
-set zrange [0:20]
-set data style points
-set pointsize 0.5
-set lmargin 0
-set bmargin 0
-set tmargin 0
-set tmargin 0
-splot @{[$obj->file_list()]}
+	my $s = IO::Socket::INET->new(
+		PeerAddr => XT3D_HOST,
+		PeerPort => XT3D_PORT,
+		Proto => 'tcp') or $obj->err("socket");
+	my ($sw, $sh) = (WIDTH, HEIGHT);
+	print $s <<EOF;
+x: $obj->{x}
+y: $obj->{y}
+z: $obj->{z}
+lx: $obj->{lx}
+ly: $obj->{ly}
+lz: $obj->{lz}
+sw: $sw
+sh: $sh
+vmode: wiredone
 EOF
-	close IN;
-	print <OUT>, <ERR>;
-	finish $h;
-}
-
-# create the list of files passed to gnuplot
-sub file_list {
-	my ($obj) = @_;
-
-	my (@files, @labels);
-
-	if (@{ $obj->{jobs} }) {
-		# foreach $job $obj->{jobs}
-		@files = glob(subst(_PATH_JOB, id => $obj->{jobs}[0]));
-	} else {
-		@files = glob(subst(_PATH_JOB, id => "*"));
-	}
-	@labels = map { 'job ' . join '', m{/(\d+)$} } @files;
-
-	my ($x, $y, $z) = @$obj{qw(x y z)};
-	$x = 0 unless -e subst(_PATH_DATA, dim => "x", pos => $x);
-	$y = 0 unless -e subst(_PATH_DATA, dim => "y", pos => $y);
-	$z = 0 unless -e subst(_PATH_DATA, dim => "z", pos => $z);
-
-	push(@files, _PATH_FREE),     push(@labels, 'free')	if -e _PATH_FREE;
-	push(@files, _PATH_DISABLED), push(@labels, 'disabled')	if -e _PATH_DISABLED;
-	push(@files, subst(_PATH_DATA, dim => "x", pos => $x)),   push(@labels, "x-plane");
-	push(@files, subst(_PATH_DATA, dim => "y", pos => $y)),   push(@labels, "y-plane");
-	push(@files, subst(_PATH_DATA, dim => "z", pos => $z)),   push(@labels, "z-plane");
-
-	my $files = "";
-	my $n;
-	for ($n = 0; $n < @files; $n++) {
-		$files .= qq{'$files[$n]' title '$labels[$n]', };
-	}
-	$files =~ s/, $//;
-	return ($files);
+	print <$s>;
+	close $s;
 }
 
 sub err {
@@ -109,11 +67,10 @@ sub err {
 }
 
 sub setview {
-	my ($obj, $rx, $rz, $sx, $sz) = @_;
-	$obj->{rx} = $rx if defined $rx && $rx =~ /^\d+$/;
-	$obj->{rz} = $rz if defined $rz && $rz =~ /^\d+$/;
-	$obj->{sx} = $sx if defined $sx && $sx =~ /^\d+$/;
-	$obj->{sz} = $sz if defined $sz && $sz =~ /^\d+$/;
+	my ($obj, $lx, $ly, $lz) = @_;
+	$obj->{lx} = $lx if defined $lx && $lx =~ /^\d+$/;
+	$obj->{ly} = $ly if defined $ly && $ly =~ /^\d+$/;
+	$obj->{lz} = $lz if defined $lz && $lz =~ /^\d+$/;
 }
 
 sub setpos {
@@ -125,7 +82,7 @@ sub setpos {
 
 sub setjob {
 	my ($obj, $id) = @_;
-	push @{ $obj->{jobs} }, $id;
+	push @{ $obj->{jobs} }, $id if defined $id && $id =~ /^\d+$/;
 }
 
 1;
