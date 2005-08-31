06c47459764b11cb6a9131e3681115b0a12a57a1
yanovich <>
2005-08-31 19:26:13 +0000

    revise for xt3dmon

--- www/plot.pl
+++ www/plot.pl
@@ -9,20 +9,19 @@ use CGI;
 my $r = shift;
 my $cgi = CGI->new();
 
-my ($job, $x, $y, $z, $rx, $rz, $sx, $sz);
+my ($job, $x, $y, $z, $lx, $ly, $lz);
 $job = $cgi->param("job");
 $x = $cgi->param("x");
 $y = $cgi->param("y");
 $z = $cgi->param("z");
-$rx = $cgi->param("rx");
-$rz = $cgi->param("rz");
-$sx = $cgi->param("sx");
-$sz = $cgi->param("sz");
+$lx = $cgi->param("lx");
+$ly = $cgi->param("ly");
+$lz = $cgi->param("lz");
 
 my $p = XTWMon::Plot->new();
 $p->setpos($x, $y, $z);
-$p->setview($rx, $rz, $sx, $sz);
-$p->setjob($job) if $job;
+$p->setview($lx, $ly, $lz);
+$p->setjob($job);
 
 $r->content_type('image/png');
-$p->gnu_plot();
+$p->print();
