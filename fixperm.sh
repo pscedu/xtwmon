f14731137349450c4818bbdda2a696b341ede3a7
yanovich <>
2005-10-22 18:26:17 +0000

    chown www/sessions correctly

--- fixperm.sh
+++ fixperm.sh
@@ -3,6 +3,7 @@
 
 ROOT=.
 
-sudo chown -R proj:proj ${ROOT}
+chown -R proj:proj ${ROOT}
+chown -R apache:apache ${ROOT}/www/sessions
 find ${ROOT} -type d -exec chmod 775 {} \;
 find ${ROOT} -type f -exec chmod 664 {} \;
